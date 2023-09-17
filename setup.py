42b832004df5931e01f8ac6f1042858240a74717
test <test@test.com>
2023-09-17 17:07:02 -0400

    update build files

--- setup.py
+++ setup.py
@@ -1,40 +1,5 @@
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-# The directory containing this file
-HERE = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+from setuptools import setup
 
 # This call to setup() does all the work
-setup(
-    name="pypes",
-    version="0.1.0",
-    description="Asynchronous piping optimization in Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://pypes.readthedocs.io/",
-    author="Mike Roumanos",
-    author_email="mroumanos@pm.me",
-    license="MIT",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Operating System :: OS Independent"
-    ],
-    packages=["pypes"],
-    include_package_data=True,
-    install_requires=[]
-)
+setup()
